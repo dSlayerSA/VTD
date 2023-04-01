9c2e9821e2b27636cc582c414b4b6ec2f02b8ada
dSlayer <luizfelipesouza123@gmail.com>
2023-04-01 15:55:24 -0300

    Tabela ok+layout botão

--- Models/EmpresaService.cs
+++ Models/EmpresaService.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 public class EmpresaService
 {
@@ -16,16 +18,29 @@ public class EmpresaService
         var json = await response.Content.ReadAsStringAsync();
         dynamic data = JsonConvert.DeserializeObject(json);
         var empresas = new List<Empresa>();
+
         foreach (var item in data.data)
         {
+
+            var segmento = JObject.Parse(item.Segmento.ToString());
+            var segmentoId = (string)segmento["$oid"];
+            var segmentoResponse = await _httpClient.GetAsync($"https://nocodebackend.azurewebsites.net/api/v1/dataset/6426ead776204041752bec02/611edbd7fd5915f2ae005dc2/{segmentoId}?apiKey=6426e9ef76204041752bebff");
+            Console.WriteLine(segmentoResponse);
+            segmentoResponse.EnsureSuccessStatusCode();
+            var segmentoJson = await segmentoResponse.Content.ReadAsStringAsync();
+            dynamic segmentoData = JsonConvert.DeserializeObject(segmentoJson);
+            var nomeSegmento = segmentoData.Nome.ToString();
+
             empresas.Add(new Empresa
             {
                 Nome = item.Nome,
                 Site = item.Site,
-                Segmento = item.Segmento.ToString()
-
+                Segmento = nomeSegmento
             });
+            Console.WriteLine($"Empresa adicionada: {item.Nome} - {item.Site} - {nomeSegmento}");
+
         }
         return empresas;
     }
+
 }
